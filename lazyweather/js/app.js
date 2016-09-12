5d1235bfe32ca61b1fc2691df974568d954eff9e
Yehyon Chung <rosie1207@gmail.com>
2016-09-12 23:12:18 +0900

    add today's weather controller and tab controller

--- lazyweather/js/app.js
+++ lazyweather/js/app.js
@@ -2,14 +2,20 @@
   'use strict';
   // var apiKey = b87a29cdc2db8cd57d8d8c85ca8c4fd8
 
-  var myApp = angular.module('myApp', []);
+  var lazyWeather = angular.module('lazyWeather', []);
 
-  myApp.controller('temperatureController', function($scope, $http){
-    $scope.$watch('search', function() {
-      fetch();
-    });
-    $scope.search = "Sherlock Holmes";
-  })
+  lazyWeather.controller('todayWeatherController', function(){
+    this.todayWeather = todayData;
+  });
 
+  var todayData = {
+    time: '2AM',
+    forcast: 'C',
+    temperature: '26',
+  };
+
+  lazyWeather.controller('TabController', function() {
+    this.tab = 1;
+  });
 
 })(this, this.angular);
