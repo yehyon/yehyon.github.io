b6afe10a93eb347357b829cbc625405aa7cb8f4c
Yehyon Chung <rosie1207@gmail.com>
2016-09-13 14:02:26 +0900

    added current weather api to lazy weather

--- lazyweather/js/app.js
+++ lazyweather/js/app.js
@@ -15,7 +15,15 @@
   };
 
   lazyWeather.controller('TabController', function() {
+    
     this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
   });
 
 })(this, this.angular);
--- lazyweather/js/controllers/CurrentWthController.js
+++ lazyweather/js/controllers/CurrentWthController.js
@@ -1,1 +1,54 @@
-currentWeatherController
+// (function(global, angular) {
+//   'use strict';
+
+//   var lazyWeather = angular.module('lazyWeather');
+
+//   lazyWeather.controller('CurrentWthController', ['$http', function($http) {
+//     var lazyWeatherCurrent = this;
+
+//     lazyWeatherCurrent.currentWeather = [];
+
+//     $http({
+//       'method': 'GET',
+//       'url': 'http://api.openweathermap.org/data/2.5/weather?id=1835847&APPID=b87a29cdc2db8cd57d8d8c85ca8c4fd8'
+//     }).then(successAjax, errorAjax);
+//     function successAjax(response) {
+//       console.log(response.data);
+//       console.log(response.data.weather[0]);
+//       // ctrl.members = response.data;
+//     }
+//     function errorAjax(response) {
+//       console.log()
+//     }
+    
+//   } ]);
+
+// })(this, this.angular);
+
+(function(global, angular) {
+  'use strict';
+
+  var lazyWeather = angular.module('lazyWeather');
+
+  lazyWeather.controller('CurrentWthController', ['$http', function($http) {
+    var lazyWeatherCurrent = this;
+
+    lazyWeatherCurrent.currentWeather = [];
+
+    $http({
+      'method': 'GET',
+      'url': 'http://api.openweathermap.org/data/2.5/weather?id=1835847&APPID=b87a29cdc2db8cd57d8d8c85ca8c4fd8'
+    }).then(successAjax, errorAjax);
+    function successAjax(response) {
+      console.log(response.data);
+      console.log(response.data.weather[0].main);
+      lazyWeatherCurrent.currentWeather = response.data;
+    }
+    function errorAjax(response) {
+      console.log()
+    }
+
+  }]);
+
+})(this, this.angular);
+
